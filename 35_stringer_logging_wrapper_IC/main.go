c7748ed8299cf0294fc06d18a3790c8243dbc5c0
Deepak Kumar <deepak.ky.delhi@gmail.com>
2023-12-24 13:42:45 +0530

    structs_funcs_interfaces_learnings

---
+++ 35_stringer_logging_wrapper_IC/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"log"
+)
+
+type book struct {
+	title string
+}
+
+func (b book) String() string {
+	return fmt.Sprintf("The book title is %s", b.title)
+}
+
+type count int
+
+func (c count) String() string {
+	return fmt.Sprintf("The value of count is %v", int(c)) // c typecasting required here
+}
+
+// Anything can be of type Stringer, it just needs to implement String() function
+func logInfo(v fmt.Stringer) {
+	log.Println("Module 138", v.String())
+}
+
+func main() {
+	b1 := book{
+		title: "How to win friends and influence people",
+	}
+
+	var pages count = 300 
+
+	//fmt.Println internally uses stringer interface
+	fmt.Println(b1)    
+	fmt.Println(pages)
+
+	//log.Println internally uses stringer interface
+	log.Println(b1)
+	log.Println(pages)
+
+	//logInfo can be passed with anything which has a type stringer
+	logInfo(b1)
+	logInfo(pages)
+
+	w := 34
+	fmt.Println(w) // does this not use String() internally ??
+	//logInfo(w) , w is of type int, int does not implement String(), hence it cannot be passed to 
+	// logInfo function
+}
+
